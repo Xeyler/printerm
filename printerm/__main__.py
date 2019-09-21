16a0dfaa0b705ed8d01be03a5cabb61e9258abc5
Brigham Campbell <me@brighamcampbell.com>
2019-09-21 04:59:29 -0600

    I don't even know anymore

--- printerm/__main__.py
+++ printerm/__main__.py
@@ -2,9 +2,8 @@ import printerm.text2vector
 import printerm.vector2gcode
 import serial
 
-ser = serial.Serial('/dev/ttyUSB0', 115200)
-print(ser.name)
 outline = printerm.text2vector.get_vector_from_char('B')
 
-print(printerm.vector2gcode.get_gcode_from_vector(outline.points, outline.contours))
-ser.write(printerm.vector2gcode.get_gcode_from_vector(outline.points, outline.contours))
+codes = printerm.vector2gcode.get_gcode_from_vector(outline.points, outline.contours)
+print(codes)
+printerm.vector2gcode.send_codes_to_printer(codes)
--- printerm/vector2gcode.py
+++ printerm/vector2gcode.py
@@ -1,15 +1,43 @@
+import serial
+import matplotlib.pyplot as plt
+
+ser = serial.Serial('/dev/ttyUSB0', 115200)
+
 def get_gcode_from_vector(points, contours):
-    gcode = ""
+    arr = [];
+
+    gcode = []
     first_point = 0
     for number_of_points_in_contour in contours:
-        for i in range(first_point, number_of_points_in_contour):
-            x = points[i][0] + 150
+        for i in range(first_point, number_of_points_in_contour + 1):
+            x = points[i][0] + 100
             y = points[i][1] + 100
 
-            gcode += "G1X" + str(x) + "Y" + str(y) + '\n'
+            arr.append((x, y))
 
-            if i is 0:
-                gcode += "G1Z1\n"
-        gcode += "G1Z5\n"
+            gcode.append("G1X" + str(x) + "Y" + str(y))
+
+            if i is first_point:
+                gcode.append("G1Z1")
+        arr.append((points[first_point][0] + 100, points[first_point][1] + 100))
+        gcode.append("G1X" + str(points[first_point][0] + 100) + "Y" + str(points[first_point][1] + 100))
+        first_point = number_of_points_in_contour + 1
+        gcode.append("G1Z5")
+
+        print(arr)
+
+        plt.plot(*zip(*arr))
+        arr = []
+
+    plt.show()
 
     return gcode
+
+def send_codes_to_printer(codes):
+    for code in codes:
+        ser.write((code + '\n').encode())
+        while True:
+            result = ser.readline()
+            print(result)
+            if result == b"ok\n":
+                break
