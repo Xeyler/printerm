8e64fe8e59ba10a171c92192cb73242fef5d7489
Brigham Campbell <me@brighamcampbell.com>
2019-09-20 18:54:00 -0600

    Added some code to text2vector. We should now be able to get FT_Outline structs from a FT_ULong unicode character code...

--- src/text2vector.c
+++ src/text2vector.c
@@ -9,10 +9,28 @@ int init(char *filepathname, int face_index) {
 	err = FT_Init_FreeType(&library);
 	if(err) {
 		printf("Error calling FT_Init_FreeType(): %d", err);
+		return 1;
 	}
 
 	err = FT_New_Face(library, face_index, 0, &face);
 	if(err) {
 		printf("Error calling FT_New_Face(): %d", err);
+		return 1;
 	}
+	return 0;
+}
+
+void get_vector_from_char(FT_ULong charcode) {
+	int glyph_index = FT_Get_Char_Index(face, charcode);
+	if(glyph_index == 0) {
+		printf("FT_Get_Char_Index(%lu) returned 0", charcode);
+		return null;
+	}	
+	 // FT_LOAD_DEFAULT looks for bitmap data first. If no bitmap data is found, it loads outline data.
+	err = FT_Load_Glyph(face, glyph_index, FT_LOAD_DEFAULT);
+	if(err) {
+		printf("Error calling FT_Load_Glyph(): %d", err);
+		return null;
+	}
+	return face->glyph->outline;
 }
